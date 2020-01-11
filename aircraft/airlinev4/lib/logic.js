ad2ccafcd79c05369e576697b1144a28cd20cd86
mojojojo20 <menonk484@gmail.com>
2020-01-11 09:56:07 +0530

    add files

--- aircraft/airlinev4/lib/logic.js
+++ aircraft/airlinev4/lib/logic.js
@@ -1,45 +1,46 @@
-/** 
- * @param {org.acme.airline.createFlight} flightData
+/**
+ * Create Flight Transaction
+ * @param {org.acme.airline.flight.createFlight} flightData
  * @transaction
  */
-    function createFlight(flightData) {
-        // 1. Get the asset registry
-        return getAssetRegistry('org.acme.airline.flight.Flight')
-            .then(function (flightRegistry) {
-                // Now add the Flight
+function    createFlight(flightData) {
+    // 1. Get the asset registry
+    return getAssetRegistry('org.acme.airline.flight.Flight')
+        .then(function(flightRegistry){
+            // Now add the Flight
 
-                // 2. Get resource factory
-                var factory = getFactory();
-                var NS = 'org.acme.airline.flight';
+            // 2. Get resource factory
+            var  factory = getFactory();
+            var  NS =  'org.acme.airline.flight';
 
-                // 3. Create the Resource instance
-                var flightId = 'AE102-05-12-18'; /// <<<< THIS IS HARD CODED - FIX IT as an exercise
+            // 3. Create the Resource instance
+            var  flightId = 'AE102-05-12-18';  /// <<<< THIS IS HARD CODED - FIX IT as an exercise
+            
+            var  flight = factory.newResource(NS,'Flight',flightId);
+            
+            // 4. Set the relationship
+            // flight.flightNumber = flightData.flightNumber;
 
-                var flight = factory.newResource(NS, 'Flight', flightId);
+            // 5. Create a new concept using the factory & set the data in it
+            var route = factory.newConcept(NS,"Route");
 
-                // 4. Set the relationship
-                flight.flightNumber = flightData.flightNumber;
+            route.originCode = flightData.origin;
+            route.DestinationCode = flightData.destination;
+            route.schedule = flightData.schedule;
+            flight.route = route;
+            // flight.aliasFlightNumber = [];
 
-                // 5. Create a new concept using the factory & set the data in it
-                var route = factory.newConcept(NS, "Route");
+            // 6. Emit the event FlightCreated
+            var event = factory.newEvent(NS, 'flightCreated');
+            event.flightId = flightId;
+            emit(event);
 
-                route.origin = flightData.origin;
-                route.destination = flightData.destination;
-                route.schedule = flightData.schedule;
-                flight.route = route;
-                flight.aliasFlightNumber = [];
-
-                // 6. Emit the event FlightCreated
-                var event = factory.newEvent(NS, 'FlightCreated');
-                event.flightId = flightId;
-                emit(event);
-
-                return flightRegistry.addAll([flight]);
-            });
-    }
+            return flightRegistry.addAll([flight]);
+        });
+}
 
 
 
-    /****
-     
-     */
+/****
+ 
+ */
